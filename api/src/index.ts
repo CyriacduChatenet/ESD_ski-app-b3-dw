33a6ccc2f522510a84861583443bb2ebd7accfe3
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-12-16 11:44:46 +0100

    [FEAT] : init front

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import { router } from './routes/router';
 import { DBconnection } from './database/dbConfig';
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 DBconnection();
 
 app.use('/api',router);
