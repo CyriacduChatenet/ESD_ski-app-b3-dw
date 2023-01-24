388155b1b96148cf2ceb007d8800cf9ebb190028
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-24 11:32:13 +0100

    [FEAT] : finish api

--- apps/client/src/pages/home/index.tsx
+++ apps/client/src/pages/home/index.tsx
@@ -1,14 +1,10 @@
 import { FC } from "react";
 
-import Layout from "@/components/layout";
-
 const HomePage: FC = () => {
   return (
-    <Layout>
-      <div id="home-page">
-        <h1>Home page</h1>
-      </div>
-    </Layout>
+    <>
+    <p>Ski app</p>
+    </>
   );
 };
 
